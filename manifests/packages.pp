7c9dd220f53ed35ad4f98aa3409a9accaf9a1bc5
Carl Caum <carl@carlcaum.com>
2014-09-19 11:44:05 -0700

    Clean up unused code
    This commit removes the robby::web class as well as commented code
    from the robby::packages clas

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,38 +1,5 @@
 class robby::packages { 
 
-  #$gems = [
-  #  'shotgun',
-  #  'sinatra',
-  #  'sinatra-croon',
-  #  'parslet',
-  #  'activeldap',
-  #  'sinatra-contrib',
-  #  'sinatra-websocket',
-  #  'data_mapper',
-  #  'rack-mobile-detect',
-  #  'dm-postgres-adapter',
-  #  'json',
-  #  'rack',
-  #  'rmagick',
-  #  'fileutils',
-  #  'thin',
-  #  'unicorn',
-  #  'kgio',
-  #  'minitest',
-  #  'atomic',
-  #  'thread_safe',
-  #  'tzinfo',
-  #  'multi_json',
-  #  'addressable',
-  #  'rack-protection',
-  #  'activesupport',
-  #  'activemodel',
-  #  'gettext',
-  #  'gettext_i18n_rails',
-  #  'builder',
-  #  'ruby-ldap'
-  #]
-
   $dev_packages = [
     'libpq-dev',
     'libldap2-dev',
@@ -44,17 +11,4 @@ class robby::packages {
   package { $dev_packages:
     ensure => installed,
   }
-
-  #package { 'passenger':
-  #  ensure   => $passenger_version,
-  #  provider => gem,
-  #  notify   => Service['apache2'],
-  #}
-
-  #package { $gems:
-  #  ensure   => installed,
-  #  provider => gem,
-  #  require  => [Alternatives['gem','ruby'],Package[$dev_packages]],
-  #  notify   => Service['apache2'],
-  #}
 }
