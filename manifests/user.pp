6acfca3e90d133c0cef68a41c82b573a884fd2ed
Carl Caum <carl@carlcaum.com>
2014-09-19 11:39:50 -0700

    Use packages to deploy application
    This commit removes vcsrepo as the prefered method to deploy the
    Robby application.  Instead, it uses a package to get the files on disk.
    Since git is no longer necessary, the deploy key has been removed and
    the ssh key for the robby user is no longer being managed.
    
    Further, this commit removes the unicorn resource definition out of the
    robby class so it can be better placed in a profile.

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,4 @@
 class robby::user (
-  $ssh_key,
   $home_directory = '/home/robby'
 ) {
 
@@ -11,27 +10,5 @@ class robby::user (
 
   group { 'robby':
     ensure => present,
-  }
-
-  file { $home_directory:
-    ensure => directory,
-    owner  => 'robby',
-    group  => 'robby',
-    mode   => 0700,
-  }
-
-  file { "${home_directory}/.ssh":
-    ensure => directory,
-    owner  => 'robby',
-    group  => 'robby',
-    mode   => 0700,
-  }
-
-  file { "${home_directory}/.ssh/id_rsa":
-    ensure  => file,
-    content => template('robby/id_rsa.erb'),
-    owner   => 'robby',
-    group   => 'robby',
-    mode    => 0600,
   }
 }
